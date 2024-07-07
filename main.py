c78a5732203109ab9b8527af5637969c21017301
IOleg-sus <guchkooleg7@gmail.com>
2024-07-07 11:40:31 +0300

    added requirements/venv

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ def main():
     print(response.text)
 
     soup = BeautifulSoup(response.text, "html.parser")
-    titles = soup.select("h2.blog-title")
+    titles = soup.select("h1.blog-title")
 
     for title in titles:
         print(title.get_text(strip=True))
+
+    with open("titles.txt", "w") as f:
+        for title in titles:
+            f.write(title.get_text() + "\n")
 
 
 if __name__ == "__main__":
